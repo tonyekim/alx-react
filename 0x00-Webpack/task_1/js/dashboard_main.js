13b3cb13e95e9907594f62987bdd2852c160e02d
tonyekim <otoobongekim@gmail.com>
2023-04-17 22:20:35 +0100

    build file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,21 +1,20 @@
-// interesting jquery import thanks to babel
+'use strict';
 import $ from 'jquery';
-// import lodash
 import _ from 'lodash';
 
-// append elements
-$('body').append(`<p>Holberton Dashboard</p>`);
-$('body').append(`<p>Dashboard data for the students</p>`);
-$('body').append(`<button>Click here to get started</button>`);
-$('body').append(`<p id="count"></p>`);
-$('body').append(`<p>Copyright - Holberton School</p>`);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-// bind lodash's debounce to the button
-$('button').on('click', _.debounce(updateCounter, 500));
+const updateCounter = () => {
+  let times = $('#count').html() || 0;
+  $('button').on('click', () => {
+    times++;
+    $('#count').html(`${times} clicks on the button`);
+  });
+};
 
-let count  = 0;
-// function to update the counter
-function updateCounter() {
-	count++;
-	$('#count').text(`${count} clicks on the button`);
-}
+_.debounce(updateCounter, 500);
+updateCounter();
