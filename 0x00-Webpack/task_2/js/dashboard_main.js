7c5fe2a4bb2c9d06fab4b74edb08c8c6968aa092
tonyekim <otoobongekim@gmail.com>
2023-04-17 22:59:15 +0100

    adding file

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,19 +1,19 @@
-'use strict';
-import '../css/main.css';
-const $ = require('jquery');
-const _ = require('lodash');
+"use strict";
+import "../css/main.css";
+const $ = require("jquery");
+const _ = require("lodash");
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>");
+$("body").append('<p id="count"></p>');
+$("body").append("<p>Copyright - Holberton School</p>");
 
 const updateCounter = () => {
-  let times = $('#count').html() || 0;
-  $('button').on('click', () => {
+  let times = $("#count").html() || 0;
+  $("button").on("click", () => {
     times++;
-    $('#count').html(`${times} clicks on the button`);
+    $("#count").html(`${times} clicks on the button`);
   });
 };
 
