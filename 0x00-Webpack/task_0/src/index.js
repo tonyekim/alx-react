6f53ca32f3158b8a95cfd3df18f0a4745d3afdb9
tonyekim <otoobongekim@gmail.com>
2023-04-18 00:23:12 +0100

    build file

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,6 @@
-// interesting jquery import thanks to babel
-import $ from 'jquery';
+'use strict';
+const $ = require('jquery');
 
-// add three different p tags to body
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<p>Copyright - Holberton School</p>');
