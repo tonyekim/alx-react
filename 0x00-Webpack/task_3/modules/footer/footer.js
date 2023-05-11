f3267494118e771f7696a1cae2f8d1ffa7565016
tonyekim <otoobongekim@gmail.com>
2023-05-11 21:38:57 +0100

    adding all the test file

--- 0x00-Webpack/task_3/modules/footer/footer.js
+++
@@ -1,7 +0,0 @@
-// interesting jquery import thanks to babel
-import $ from "jquery";
-// styles
-import "./footer.css";
-
-// append elements
-$("body").append(`<p>Copyright - Holberton School</p>`);
--- 0x00-Webpack/task_3/modules/header/header.js
+++
@@ -1,9 +0,0 @@
-// interesting jquery import thanks to babel
-import $ from "jquery";
-// styles
-import "./header.css";
-
-// append elements
-$("body").append(`<div id="logo"></div>`);
-$("body").append(`<p>Holberton Dashboard</p>`);
-console.log("Init header");
